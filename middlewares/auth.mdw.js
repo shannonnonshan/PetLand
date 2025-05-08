759a5da2d019ee15912c372006d0032b27fa908d
shannonnonshan <khanhminhdoan04@gmail.com>
2025-05-08 14:23:43 +0700

    fix login

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -1,8 +1,10 @@
 
 
 export default function auth(req, res, next) {
+    console.log(req.originalUrl);
     if (!req.session.auth || !req.session.authUser) {
         req.session.retUrl = req.originalUrl || null;
+        console.log(req.originalUrl);
         console.log(req.session.retUrl);
         return res.redirect('/user/popupLogin');
     }
